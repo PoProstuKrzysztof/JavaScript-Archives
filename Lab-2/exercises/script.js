798b4e918af6fa30cfa12c8872c02ba54f7c0047
JustChris <krzysiek.palonek@gmail.com>
2023-10-17 13:03:38 +0200

    17.10

---
+++ Lab-2/exercises/script.js
@@ -0,0 +1,21 @@
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(1,2));
+
+const operation = function(a,b){
+    return a * b;
+}
+
+console.log(operation(1,2));
+operation = sum;
+
+let filter = val => val === 3;
+console.log(filter(3));
+let arr = [2,3,4,5,6,7];
+function select(Array, filter){
+    let result = [];
+    for(let item of array){
+    }
+}
